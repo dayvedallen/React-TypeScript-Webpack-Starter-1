208be0a063bb4098aad871afca4dd259d46a9846
dayved <dayvedallen@gmail.com>
2023-06-29 12:23:46 -0400

    ran yarn

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { Counter } from './Counter'
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>
+        Edit React Typescript Webpack starter tempate - {'  '}
+        {process.env.NODE_ENV} {process.env.name}
+      </h1>
       <Counter />
     </>
   )
